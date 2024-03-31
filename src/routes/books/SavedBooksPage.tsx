be75536793e455b099c82236e2301ef1c7eb14a5
ckdev88 <ckdev88@outlook.com>
2024-03-31 11:29:39 -0300

    small type tweaks

--- src/routes/books/SavedBooksPage.tsx
+++ src/routes/books/SavedBooksPage.tsx
@@ -1,13 +1,10 @@
-import { useNavigate,Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import removeBookFromSaved from '../../stores/removeBookFromSaved'
 
 type Author = string
-interface Authors {
-	author: Author
-}
 interface Book {
 	id: number
-	authors: [Authors]
+	authors: [Author]
 	cover?: string
 	date_published: string
 	image?: string
@@ -17,12 +14,9 @@ interface Book {
 	title?: string
 	title_short: string
 }
-interface Books {
-	book: Book
-}
+type Books = [Book]
 
 export default function SavedBooksPage() {
-	const navigate = useNavigate()
 	let hasbooks = false // TODO: make dynamic with either Ref or State
 
 	let savedbooks: Books = []
@@ -33,49 +27,107 @@ export default function SavedBooksPage() {
 		// TODO: manage global state array
 	}
 
-	function showSavedbooks() {
-		return savedbooks.map((book) => {
-			return (
-				<>
-					<article key={book.id} className="book-summary">
-						<header>
-							<aside className="cover">
-								<img src={book.image!=='https://images.isbndb.com/coversnull'&&book.image?book.image:'/logo.svg'} alt="" />
-							</aside>
-							<div className="in-short">
-								<h2>
-									{book.title_short}
-									<sub>
-										{book.authors.map((author: Author, index: number) => {
-											return (
-												<span key={index}>
-													{author}
-													<br />
-												</span>
-											)
-										})}
-									</sub>
-								</h2>
-								{book.date_published}
-								<br />
-								{book.pages} pages
+	const savedbookslist = savedbooks.map((book: Book) => {
+		return (
+			<>
+				<article key={book.id} className="book-summary">
+					<header>
+						<aside className="cover">
+							<img
+								src={
+									book.image !== 'https://images.isbndb.com/coversnull' && book.image
+										? book.image
+										: '/logo.svg'
+								}
+								alt=""
+							/>
+						</aside>
+						<div className="in-short">
+							<h2>
+								{book.title_short}
+								<sub>
+									{book.authors.map((author, index: number) => {
+										return (
+											<span key={index}>
+												{author}
+												<br />
+											</span>
+										)
+									})}
+								</sub>
+							</h2>
+							{book.date_published}
+							<br />
+							{book.pages} pages
+						</div>
+					</header>
+					<footer>
+						<div className="marks">
+							<div className="mark">
+								<a onClick={() => removeBook(book.id)}>
+									<span className="icon icon-remove"></span>Remove from my books
+								</a>
 							</div>
-						</header>
-						<footer>
-							<div className="marks">
-								<div className="mark">
-									<a onClick={()=>removeBook(book.id)}>
-										<span className="icon icon-remove"></span>Remove from my books
-									</a>
+						</div>
+						<hr />
+					</footer>
+				</article>
+			</>
+		)
+	})
+
+	/* function showSavedbooks() {
+		if (savedbooks.length > 0) {
+			return savedbooks.map((book) => {
+				return (
+					<>
+						<article key={book.id} className="book-summary">
+							<header>
+								<aside className="cover">
+									<img
+										src={
+											book.image !== 'https://images.isbndb.com/coversnull' && book.image
+												? book.image
+												: '/logo.svg'
+										}
+										alt=""
+									/>
+								</aside>
+								<div className="in-short">
+									<h2>
+										{book.title_short}
+										<sub>
+											{book.authors.map((author: Author, index: number) => {
+												return (
+													<span key={index}>
+														{author}
+														<br />
+													</span>
+												)
+											})}
+										</sub>
+									</h2>
+									{book.date_published}
+									<br />
+									{book.pages} pages
 								</div>
-							</div>
-							<hr />
-						</footer>
-					</article>
-				</>
-			)
-		})
-	}
+							</header>
+							<footer>
+								<div className="marks">
+									<div className="mark">
+										<a onClick={() => removeBook(book.id)}>
+											<span className="icon icon-remove"></span>Remove from my books
+										</a>
+									</div>
+								</div>
+								<hr />
+							</footer>
+						</article>
+					</>
+				)
+			})
+		}
+	} */
 
 	return (
 		<>
@@ -87,12 +139,12 @@ export default function SavedBooksPage() {
 			<div className={!hasbooks && 'dnone'}>
 				<h4>No books added yet, find them and add them.</h4>
 				<p>
-					<Link to={ '/search' } className="wauto">
+					<Link to={'/search'} className="wauto">
 						Search
 					</Link>
 				</p>
 			</div>
-			<div>{showSavedbooks()}</div>
+			<div>{savedbookslist}</div>
 		</>
 	)
 }
