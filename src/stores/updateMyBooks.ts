e9c306c27758305a53a22c28a27cdaa51307a0f1
ckdev88 <ckdev88@outlook.com>
2024-04-01 00:06:51 -0300

    cleanup

--- src/stores/updateMyBooks.ts
+++ src/stores/updateMyBooks.ts
@@ -4,7 +4,5 @@ async function updateMyBooks(myBooksNew: string) {
 	const { data, error } = await supabase.auth.updateUser({
 		data: { myBooks: myBooksNew },
 	})
-	if (!error) console.log('updated', data.user.user_metadata.screenname, data.user.user_metadata.myBooks, 'si')
-	else console.log('error updating mybooks:', error)
 }
 export default updateMyBooks
