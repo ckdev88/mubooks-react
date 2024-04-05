b3958d8110048398c7c3319af27d3e07b6872dd4
ckdev88 <ckdev88@outlook.com>
2024-04-05 11:48:17 -0300

    replace browserrouter with hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter basename='/mubooks'>
+		<HashRouter basename='/mubooks'>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 )
