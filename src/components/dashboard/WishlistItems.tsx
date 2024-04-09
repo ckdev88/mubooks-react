53cb7569ce181c66bf3fd1a536e71a4846e41d15
ckdev88 <ckdev88@outlook.com>
2024-04-09 13:56:58 -0300

    tweak deck styling dashboard, applied to wishlist-section

--- src/components/dashboard/WishlistItems.tsx
+++ src/components/dashboard/WishlistItems.tsx
@@ -1,19 +1,19 @@
 import { useContext } from 'react'
 import { AppContext } from '../../App'
 
-
 export default function WishlistItems() {
 	function DeckCovers(books: string) {
 		let booksParsed = JSON.parse(books)
-		return (
-			booksParsed.slice(0, 6).map((book: Book, index: number) => {
-				var img = 'https://images.isbndb.com/covers' + book.img + '.jpg'
-				return (
-					<article className='book-cover' key={index} style={{ backgroundColor: 'hotpink', zIndex: 10 - index }}>
-						<img src={img} />
-					</article>)
-			})
-		)
+		console.log('typeof bookParsed', typeof booksParsed)
+		if (typeof booksParsed !== 'object') booksParsed = JSON.parse(booksParsed)
+		return booksParsed.slice(-6).map((book: Book, index: number) => {
+			var img = 'https://images.isbndb.com/covers' + book.img + '.jpg'
+			return (
+				<article className="book-cover" key={index} style={{ zIndex: 10 - index }}>
+					<img src={img} />
+				</article>
+			)
+		})
 	}
 	const { userMyBooks } = useContext(AppContext)
 	let hasbooks = false
@@ -21,23 +21,20 @@ export default function WishlistItems() {
 
 	return (
 		<>
-			{
-				hasbooks ? (
-					<main className='wishlist deck'>
-						<div className='deck-container'>
-							{DeckCovers(userMyBooks)}</div>
-					</main>
-				) : (
-					<main className="toadd">
-						<aside>
-							<button>
-								<img src="img/icon-wishlist.png" />
-							</button>
-						</aside>
-						Next in line.
-					</main>
-				)
-			}
+			{hasbooks ? (
+				<main className="wishlist deck">
+					<div className="deck-container">{DeckCovers(userMyBooks)}</div>
+				</main>
+			) : (
+				<main className="toadd">
+					<aside>
+						<button>
+							<img src="img/icon-wishlist.png" />
+						</button>
+					</aside>
+					Next in line.
+				</main>
+			)}
 		</>
 	)
 }
