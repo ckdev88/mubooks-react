2fb7c942541ef15c6006b73ecc9d5fbf9c8f6d65
ckdev88 <ckdev88@outlook.com>
2024-04-09 18:12:11 -0300

    add deck for dashboard > saved books

--- src/components/dashboard/SavedItems.tsx
+++ src/components/dashboard/SavedItems.tsx
@@ -1,22 +1,46 @@
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { AppContext } from '../../App'
 
 // TODO: add deck of "cards" to show first n of saved books (covers)
 
 const SavedItems = () => {
+	const { userMyBooks } = useContext(AppContext)
+	let hasbooks: boolean = false
+	let booksParsed: Books = JSON.parse(userMyBooks)
+	if (typeof (booksParsed) !== 'object') booksParsed = JSON.parse(booksParsed)
+	const booksarr = booksParsed
+	if (booksarr.length > 0) hasbooks = true
+	function DeckCovers(booksarr: Books) {
+		return booksarr.slice(-6).map((book: Book, index: number) => {
+			var img = 'https://images.isbndb.com/covers' + book.img + '.jpg'
+			return (
+				<article className='book-cover' key={index} style={{ zIndex: 10 - index }}><img src={img} /></article>
+			)
+		})
+	}
+
 	const navigate = useNavigate()
 	function goSavedBooks() {
 		navigate('/saved-books')
 	}
+
 	return (
 		<>
-			<main className="toadd">
-				<aside>
-					<button onClick={() => goSavedBooks()}>
-						<img src="img/save-books-icon.png" />
-					</button>
-				</aside>
-				Let's start saving books.
-			</main>
+			{hasbooks ? (
+				<main className='saved deck'>
+					<div className='deck-container'>{DeckCovers(booksarr)}</div>
+				</main>
+			) : (
+				<main className="toadd">
+					<aside>
+						<button onClick={() => goSavedBooks()}>
+							<img src="img/save-books-icon.png" />
+						</button>
+					</aside>
+					Let's start saving books.
+				</main>
+			)}
 		</>
 	)
 }
