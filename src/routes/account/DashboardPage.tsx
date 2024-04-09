5060d1618ef79c013f472c5a7535ba8ca9e4760f
ckdev88 <ckdev88@outlook.com>
2024-04-09 02:12:23 -0300

    cleanup after adding new features

--- src/routes/account/DashboardPage.tsx
+++ src/routes/account/DashboardPage.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
 import QuoteCard from '../../components/QuoteCard'
 import Reading from '../../components/dashboard/Reading'
 import Saved from '../../components/dashboard/Saved'
