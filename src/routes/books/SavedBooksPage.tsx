68d8dd7019843a91a0a35949b661814798cb64d3
ckdev88 <ckdev88@outlook.com>
2024-04-13 16:02:37 -0300

    clarify code hiding block to add books on books being present

--- src/routes/books/SavedBooksPage.tsx
+++ src/routes/books/SavedBooksPage.tsx
@@ -2,22 +2,21 @@ import { Link } from 'react-router-dom'
 import BooksOverviewPage from './BooksOverviewPage'
 
 export default function SavedBooksPage() {
-	let hasbooks = false // TODO: make dynamic with either Ref or State
-
 	let savedbooks: Books
+	let hasBooks: boolean = false
 	if (localStorage.getItem('MyBooks') === 'undefined') {
 		savedbooks = []
-	}
-	else savedbooks = JSON.parse(localStorage.getItem('MyBooks') as string)
+	} else savedbooks = JSON.parse(localStorage.getItem('MyBooks') as string)
+	if (savedbooks.length > 0) hasBooks = true
 
 	return (
 		<>
 			<h1>My Books</h1>
 			<p>
-				An overview of my saved books, this includes books that are favorited, read and finished,
-				the wishlist and the book currently reading.
+				An overview of my saved books, this includes books that are favorited, read and
+				finished, the wishlist and the book currently reading.
 			</p>
-			<div className={!hasbooks && 'dnone'}>
+			<div className={hasBooks === true ? 'dnone' : 'dblock'}>
 				<h4>No books added yet, find them and add them.</h4>
 				<p>
 					<Link to={'/search'} className="wauto">
