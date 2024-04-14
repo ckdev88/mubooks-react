adbba91c5f575994ed359fc1780ecb975ce5e69d
ckdev88 <ckdev88@outlook.com>
2024-04-14 07:45:08 -0300

    improve redirect on first load

--- src/routes/RootPage.tsx
+++ src/routes/RootPage.tsx
@@ -1,14 +1,20 @@
 import NavWrapper from '../components/NavWrapper'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import { localStorageKey } from '../../utils/supabase'
 
 const RootPage = () => {
-	const loggedin: boolean = false
+	let loggedin: boolean = false
 	const navigate = useNavigate()
 
+	if(JSON.parse(localStorage.getItem(localStorageKey) as string).user.aud==='authenticated')loggedin=true
+
+	let navigateTo: string = '/account/login'
+	if (loggedin) navigateTo = '/dashboard'
+
 	useEffect(() => {
-		if (loggedin) navigate('/dashboard')
-		else navigate('/account/login')
+		if (loggedin) navigate(navigateTo)
+		else navigate(navigateTo)
 	}, [])
 
 	return (
@@ -17,11 +23,9 @@ const RootPage = () => {
 				<NavWrapper />
 			</header>
 			<main id="main" className="textwrapper">
-				<div className="textwrapper">Redirecting to wherever you should be right now.</div>
+				<div className="textwrapper">Redirecting to wherever you should be right now...<br />
+					<Link to={navigateTo}>Click here</Link> if you're not redirected automatically.</div>
 			</main>
-			<footer id="footer" className="textWrapper">
-				Y'know?
-			</footer>
 		</>
 	)
 }
