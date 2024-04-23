534cdbdf578b72ce24d4580c91679424a717243c
ckdev88 <ckdev88@outlook.com>
2024-04-23 00:20:03 -0300

    make rotate a bit more robust by asking ID in time

--- src/hooks/useCardRotate.ts
+++ src/hooks/useCardRotate.ts
@@ -1,12 +1,14 @@
 const useCardRotate = () => {
-	const cardSignup: HTMLElement | null = document.getElementById('card-signup')
-	const cardRecover: HTMLElement | null = document.getElementById('card-recover')
 	function recover() {
+		const cardSignup: HTMLElement | null = document.getElementById('card-signup')
+		const cardRecover: HTMLElement | null = document.getElementById('card-recover')
 		document.getElementById('cards-draaideur')?.classList.add('rotate')
 		if (cardSignup) cardSignup.style.display = 'none'
 		if (cardRecover) cardRecover.style.display = 'block'
 	}
 	function signup() {
+		const cardRecover: HTMLElement | null = document.getElementById('card-recover')
+		const cardSignup: HTMLElement | null = document.getElementById('card-signup')
 		document.getElementById('cards-draaideur')?.classList.add('rotate')
 		if (cardRecover) cardRecover.style.display = 'none'
 		if (cardSignup) cardSignup.style.display = 'block'
@@ -16,12 +18,9 @@ const useCardRotate = () => {
 	}
 	function see() {
 		document.getElementById('cards-draaideur')?.classList.remove('rotate')
-		// document.getElementById('card-see-profile').style.display = 'block'
 	}
 	function change() {
 		document.getElementById('cards-draaideur')?.classList.add('rotate')
-		// document.getElementById('card-change-profile').style.display = 'block'
-		// document.getElementById('card-see-profile').style.display = 'none'
 	}
 
 	return { signup, recover, login, see, change }
