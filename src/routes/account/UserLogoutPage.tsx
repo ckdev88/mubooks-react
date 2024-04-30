ec794c371ea37733d1a493bde447c2932b4c293c
ckdev88 <ckdev88@outlook.com>
2024-04-29 21:04:05 -0300

    simplify logout, de-abstract

--- src/routes/account/UserLogoutPage.tsx
+++ src/routes/account/UserLogoutPage.tsx
@@ -1,40 +1,28 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-// import { UserLogout } from '../../helpers/AuthHelpers'
-import { useContext } from "react"
+import { useContext } from 'react'
 import { AppContext } from '../../App'
 import { supabase } from '../../../utils/supabase'
-
-const UserLogout = async () => {
-	let { error } = await supabase.auth.signOut()
-	if (error === null) {
-		console.log('logged out')
-	} else {
-		return error
-	}
-	return error
-}
 
 export default function UserLogoutPage() {
 	const { setUserIsLoggedIn } = useContext(AppContext)
 	const navigate = useNavigate()
-	async function processLogout() {
-		setUserIsLoggedIn(false)
-		UserLogout()
-		// TODO: for now we assume there are no errors... this will need to change
 
-		setTimeout(() => {
-			navigate('/account/login')
-		}, 600)
+	async function dologout():Promise<void> {
+		await supabase.auth.signOut()
 	}
-	useEffect(() => { processLogout() }, [])
+
+	useEffect(() => {
+		dologout()
+		localStorage.clear()
+		setUserIsLoggedIn(false)
+		navigate('/account/login')
+	}, [])
 
 	return (
 		<>
 			<h1>Logging out.</h1>
-			<p>
-				Redirecting to login page...
-			</p>
+			<p>Redirecting to login page...</p>
 		</>
 	)
 }
