15e437b54e85ca580ab2f6769097005d5c3538dc
ckdev88 <ckdev88@outlook.com>
2024-05-15 19:48:59 -0300

    add auth-confirm pages to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Routes, Route } from 'react-router-dom'
-import { createContext, useState } from 'react'
-import { localStorageKey } from '../utils/supabase'
 import './functions/miscEventListeners.ts'
 import AddBookPage from './routes/books/AddBookPage'
+import AuthConfirm from './routes/auth/Confirm.tsx'
 import CheckMailNewAccountPage from './routes/account/CheckMailNewAccountPage'
 import CheckMailPasswordPage from './routes/account/CheckMailPasswordPage'
 import ClearMyBooks from './routes/books/ClearMyBooks'
@@ -18,6 +16,9 @@ import UserLoginPage from './routes/account/UserLoginPage'
 import UserLogoutPage from './routes/account/UserLogoutPage'
 import UserProfilePage from './routes/account/UserProfilePage'
 import WishlistPage from './routes/books/WishlistPage'
+import { Routes, Route } from 'react-router-dom'
+import { createContext, useState } from 'react'
+import { localStorageKey } from '../utils/supabase'
 
 export const AppContext = createContext<AppContextType>({} as AppContextType)
 
@@ -94,22 +95,23 @@ const App = () => {
 						<Route path="/account/login" Component={UserLoginPage} />
 						<Route path="/account/forgotpassword" Component={CheckMailPasswordPage} />
 						<Route path="/account/logout" Component={UserLogoutPage} />
-						{ userIsLoggedIn && 
+						<Route path="/auth/confirm" Component={AuthConfirm} />
+						{userIsLoggedIn && (
 							<>
-						<Route path="/account/new" Component={CheckMailNewAccountPage} />
-						<Route path="/account/profile" Component={UserProfilePage} />
-						<Route path="/account/*" Component={UserLoginPage} />
-						<Route path="/dashboard" Component={DashboardPage} />
-						<Route path="/search" Component={SearchPage} />
-						<Route path="/add-book" Component={AddBookPage} />
-						<Route path="/saved-books" Component={SavedBooksPage} />
-						<Route path="/wishlist" Component={WishlistPage} />
-						<Route path="/reading" Component={ReadingPage} />
-						<Route path="/finished" Component={FinishedPage} />
-						<Route path="/favorites" Component={FavoritesPage} />
-						<Route path="/clear-my-books" Component={ClearMyBooks} />
-						</>
-						}
+								<Route path="/account/new" Component={CheckMailNewAccountPage} />
+								<Route path="/account/profile" Component={UserProfilePage} />
+								<Route path="/account/*" Component={UserLoginPage} />
+								<Route path="/dashboard" Component={DashboardPage} />
+								<Route path="/search" Component={SearchPage} />
+								<Route path="/add-book" Component={AddBookPage} />
+								<Route path="/saved-books" Component={SavedBooksPage} />
+								<Route path="/wishlist" Component={WishlistPage} />
+								<Route path="/reading" Component={ReadingPage} />
+								<Route path="/finished" Component={FinishedPage} />
+								<Route path="/favorites" Component={FavoritesPage} />
+								<Route path="/clear-my-books" Component={ClearMyBooks} />
+							</>
+						)}
 					</Routes>
 				</main>
 			</AppContext.Provider>
