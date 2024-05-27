a40556e4a7b493ff66df774765900aad8f320d93
ckdev88 <ckdev88@outlook.com>
2024-05-27 14:32:49 -0300

    apply keyboard event listeners for testing purposes only on localhost

--- src/functions/miscEventListeners.ts
+++ src/functions/miscEventListeners.ts
@@ -1,16 +1,17 @@
-
-addEventListener('keydown', (keyboardEvent: KeyboardEvent) => {
-	if (keyboardEvent.ctrlKey && keyboardEvent.key === 'k') {
-		keyboardEvent.preventDefault()
-		if (location.pathname !== '/mubooks') location.pathname = '/mubooks'
-		if (location.hash.split('/').pop() !== 'search') location.hash = '#/search'
-	}
-	if (keyboardEvent.key === 'j') window.scrollBy(0, 64)
-	else if (keyboardEvent.key === 'k') window.scrollBy(0, -64)
-	else if (keyboardEvent.key === '/') {
-		keyboardEvent.preventDefault()
-		// TODO: remove setting harry potter when done testing
-		document.getElementById('search_term')?.setAttribute('value', 'harry potter')
-		document.getElementById('search_term')?.focus()
-	}
-})
+if (location.hostname === 'localhost') {
+	addEventListener('keydown', (keyboardEvent: KeyboardEvent) => {
+		if (keyboardEvent.ctrlKey && keyboardEvent.key === 'k') {
+			keyboardEvent.preventDefault()
+			if (location.pathname !== '/mubooks') location.pathname = '/mubooks'
+			if (location.hash.split('/').pop() !== 'search') location.hash = '#/search'
+		}
+		if (keyboardEvent.key === 'j') window.scrollBy(0, 64)
+		else if (keyboardEvent.key === 'k') window.scrollBy(0, -64)
+		else if (keyboardEvent.key === '/') {
+			keyboardEvent.preventDefault()
+			// TODO: remove setting harry potter when done testing
+			document.getElementById('search_term')?.setAttribute('value', 'harry potter')
+			document.getElementById('search_term')?.focus()
+		}
+	})
+}
