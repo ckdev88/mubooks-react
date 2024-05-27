761376177f105111730f7230da6750a5437a8db1
ckdev88 <ckdev88@outlook.com>
2024-05-27 19:27:58 -0300

    remove done DONE note

--- src/functions/miscEventListeners.ts
+++ src/functions/miscEventListeners.ts
@@ -9,7 +9,6 @@ if (location.hostname === 'localhost') {
 		else if (keyboardEvent.key === 'k') window.scrollBy(0, -64)
 		else if (keyboardEvent.key === '/') {
 			keyboardEvent.preventDefault()
-			// TODO: remove setting harry potter when done testing
 			document.getElementById('search_term')?.setAttribute('value', 'harry potter')
 			document.getElementById('search_term')?.focus()
 		}
