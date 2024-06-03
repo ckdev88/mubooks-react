836506239b3063da8a38dfa6fd1a6d288d979cb8
ckdev88 <ckdev88@outlook.com>
2024-06-03 14:27:44 -0300

    limit initial shown tropes in search to 5, add expand/collapse button

--- src/components/SearchTropes.tsx
+++ src/components/SearchTropes.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react'
 const SearchTropes = (bookid: Id, tropes: BookTropes) => {
+	const [showMore, setShowMore] = useState(false)
+
+	function showMoreToggle() {
+		setShowMore(!showMore)
+	}
+
 	if (tropes === undefined) return
 	return (
 		<ul className="tropes clr mb0">
-			{tropes.map((trope, index) => (
-				<li className="trope badge" key={'trope' + bookid + index}>
-					{trope}
-				</li>
-			))}
+			{tropes.map((trope, index) => {
+				if (showMore) {
+					return (
+						<li className="trope badge" key={'trope' + bookid + index}>
+							{trope}
+						</li>
+					)
+				} else if (index < 5) {
+					return (
+						<li className="trope badge" key={'trope' + bookid + index}>
+							{trope}
+						</li>
+					)
+				}
+			})}
+			<li className="trope_more">
+				<button onClick={() => showMoreToggle()} className="btn-sm mb0">
+					{showMore ? '<' : '...'}
+				</button>
+			</li>
 		</ul>
 	)
 }
