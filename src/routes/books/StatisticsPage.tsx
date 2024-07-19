f6b554cdc669231a9fd9664257b22edb51382a03
ckdev88 <ckdev88@outlook.com>
2024-07-19 04:09:24 -0300

    statistics: add books finished counter per year ... basics

--- src/routes/books/StatisticsPage.tsx
+++ src/routes/books/StatisticsPage.tsx
@@ -5,21 +5,51 @@ const pageTitle = 'Mu Statistics'
 const currentYear = new Date().getFullYear()
 
 const StatisticsPage = () => {
-	const { setNavTitle } = useContext(AppContext)
+	const { userMyBooks, setNavTitle } = useContext(AppContext)
 	useEffect(() => {
 		setNavTitle(pageTitle)
 	}, [setNavTitle])
+
+	const getAmountBooksFinished = (year: number): number => {
+		let amount = 0
+		const triggerYearStart = year * 10000
+		const triggerYearEnd = triggerYearStart + 10000
+		if (year === 2020) console.log('get finished books between', triggerYearStart, '<', triggerYearEnd)
+		for (let i = 0; i < userMyBooks.length; i++) {
+			if (userMyBooks[i].date_finished === undefined) continue
+			if (
+				Number(userMyBooks[i].date_finished) > triggerYearStart &&
+				Number(userMyBooks[i].date_finished) < triggerYearEnd
+			)
+				amount++
+		}
+		return amount
+	}
 
 	return (
 		<>
 			<h1>Mu Statistics</h1>
 			<h2>Your numbers for {currentYear}</h2>
-			<p>coming soon</p>
-			<h3>Books finished</h3>
+			<h3>Books finished: {getAmountBooksFinished(currentYear)}</h3>
 			<p>coming soon</p>
 			<h3>Reading speed in books and pages</h3>
 			<p>coming soon</p>
 			<h3>Average day to finish a book</h3>
+			<hr />
+			<h2>Your numbers for {currentYear - 1}</h2>
+			<h3>Books finished: {getAmountBooksFinished(currentYear - 1)}</h3>
+			<hr />
+			<h2>Your numbers for {currentYear - 2}</h2>
+			<h3>Books finished: {getAmountBooksFinished(currentYear - 2)}</h3>
+			<hr />
+			<h2>Your numbers for {currentYear - 3}</h2>
+			<h3>Books finished: {getAmountBooksFinished(currentYear - 3)}</h3>
+			<hr />
+			<h2>Your numbers for {currentYear - 4}</h2>
+			<h3>Books finished: {getAmountBooksFinished(currentYear - 4)}</h3>
+			<hr />
+			<h2>Your numbers for {currentYear - 5}</h2>
+			<h3>Books finished: {getAmountBooksFinished(currentYear - 5)}</h3>
 			<hr />
 		</>
 	)
