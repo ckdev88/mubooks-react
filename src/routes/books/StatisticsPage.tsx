0f45e5dac4e2a983b8b067bffdd570a2541aa39f
ckdev88 <ckdev88@outlook.com>
2024-07-21 13:33:00 -0300

    statistics: simplify current presentation

--- src/routes/books/StatisticsPage.tsx
+++ src/routes/books/StatisticsPage.tsx
@@ -57,71 +57,89 @@ const StatisticsPage = () => {
 	return (
 		<>
 			<h1>Mu Statistics</h1>
-			<h2>Your numbers for {currentYear}</h2>
-			<h3>Books finished: {getAmount(currentYear, 'books')}</h3>
-			<h3>Pages finished: {getAmount(currentYear, 'pages')}</h3>
-			<h3>Average days per book: {getAmount(currentYear, 'daysperbook')}</h3>
-			<h3>Average pages per day: {getAmount(currentYear, 'pagesperday')}</h3>
+			<h2>{currentYear}</h2>
+			Books finished: {getAmount(currentYear, 'books')}<br/>
+			Pages finished: {getAmount(currentYear, 'pages')}<br/>
+			Average days per book: {getAmount(currentYear, 'daysperbook')}<br/>
+			Average pages per day: {getAmount(currentYear, 'pagesperday')}<br/>
 			<p>coming soon</p>
 			<h3>Reading speed in books and pages</h3>
 			<p>coming soon</p>
 			<h3>Average day to finish a book</h3>
 			<hr />
-			<h2>Your numbers for {currentYear - 1}</h2>
+			<h2>{currentYear - 1}</h2>
 			{getAmount(currentYear - 1, 'books') > 0 ? (
 				<>
-					<h3>Books finished: {getAmount(currentYear - 1, 'books')}</h3>
-					<h3>Pages finished: {getAmount(currentYear - 1, 'pages')}</h3>
-					<h3>Average days per book: {getAmount(currentYear - 1, 'daysperbook')}</h3>
-					<h3>Average pages per day: {getAmount(currentYear - 1, 'pagesperday')}</h3>
+					Books finished: {getAmount(currentYear - 1, 'books')}
+					<br />
+					Pages finished: {getAmount(currentYear - 1, 'pages')}
+					<br />
+					Average days per book: {getAmount(currentYear - 1, 'daysperbook')}
+					<br />
+					Average pages per day: {getAmount(currentYear - 1, 'pagesperday')}
+					<br />
 				</>
 			) : (
 				<>Nada</>
 			)}
 			<hr />
-			<h2>Your numbers for {currentYear - 2}</h2>
+			<h2>{currentYear - 2}</h2>
 			{getAmount(currentYear - 2, 'books') > 0 ? (
-				<>
-					<h3>Books finished: {getAmount(currentYear - 2, 'books')}</h3>
-					<h3>Pages finished: {getAmount(currentYear - 2, 'pages')}</h3>
-					<h3>Average days per book: {getAmount(currentYear - 2, 'daysperbook')}</h3>
-					<h3>Average pages per day: {getAmount(currentYear - 2, 'pagesperday')}</h3>
-				</>
+				<p>
+					Books finished: {getAmount(currentYear - 2, 'books')}
+					<br />
+					Pages finished: {getAmount(currentYear - 2, 'pages')}
+					<br />
+					Average days per book: {getAmount(currentYear - 2, 'daysperbook')}
+					<br />
+					Average pages per day: {getAmount(currentYear - 2, 'pagesperday')}
+					<br />
+				</p>
 			) : (
-				<>Nada</>
+				<p>No numbers for {currentYear - 2}</p>
 			)}
 			<hr />
-			<h2>Your numbers for {currentYear - 3}</h2>
+			<h2>{currentYear - 3}</h2>
 			{getAmount(currentYear - 3, 'books') > 0 ? (
-				<>
-					<h3>Books finished: {getAmount(currentYear - 3, 'books')}</h3>
-					<h3>Pages finished: {getAmount(currentYear - 3, 'pages')}</h3>
-					<h3>Average days per book: {getAmount(currentYear - 3, 'daysperbook')}</h3>
-					<h3>Average pages per day: {getAmount(currentYear - 3, 'pagesperday')}</h3>
-				</>
+				<p>
+					Books finished: {getAmount(currentYear - 3, 'books')}
+					<br />
+					Pages finished: {getAmount(currentYear - 3, 'pages')}
+					<br />
+					Average days per book: {getAmount(currentYear - 3, 'daysperbook')}
+					<br />
+					Average pages per day: {getAmount(currentYear - 3, 'pagesperday')}
+					<br />
+				</p>
 			) : (
 				<>Nada</>
 			)}
 			<hr />
-			<h2>Your numbers for {currentYear - 4}</h2>
+			<h2>{currentYear - 4}</h2>
 			{getAmount(currentYear - 4, 'books') > 0 ? (
-				<>
-					<h3>Books finished: {getAmount(currentYear - 4, 'books')}</h3>
-					<h3>Pages finished: {getAmount(currentYear - 4, 'pages')}</h3>
-					<h3>Average days per book: {getAmount(currentYear - 4, 'daysperbook')}</h3>
-					<h3>Average pages per day: {getAmount(currentYear - 4, 'pagesperday')}</h3>
-				</>
+				<p>
+					Books finished: {getAmount(currentYear - 4, 'books')}
+					<br />
+					Pages finished: {getAmount(currentYear - 4, 'pages')}
+					<br />
+					Average days per book: {getAmount(currentYear - 4, 'daysperbook')}
+					<br />
+					Average pages per day: {getAmount(currentYear - 4, 'pagesperday')}
+				</p>
 			) : (
 				<>Nada</>
 			)}
 			<hr />
-			<h2>Your numbers for {currentYear - 5}</h2>
+			<h2>{currentYear - 5}</h2>
 			{getAmount(currentYear - 5, 'books') > 0 ? (
 				<>
-					<h3>Books finished: {getAmount(currentYear - 5, 'books')}</h3>
-					<h3>Pages finished: {getAmount(currentYear - 5, 'pages')}</h3>
-					<h3>Average days per book: {getAmount(currentYear - 5, 'daysperbook')}</h3>
-					<h3>Average pages per day: {getAmount(currentYear - 5, 'pagesperday')}</h3>
+					Books finished: {getAmount(currentYear - 5, 'books')}
+					<br />
+					Pages finished: {getAmount(currentYear - 5, 'pages')}
+					<br />
+					Average days per book: {getAmount(currentYear - 5, 'daysperbook')}
+					<br />
+					Average pages per day: {getAmount(currentYear - 5, 'pagesperday')}
 				</>
 			) : (
 				<>Nada</>
