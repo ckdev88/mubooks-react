fabda049cc087c17c37c1862447896b18e9a51ce
ckdev88 <ckdev88@outlook.com>
2024-08-12 14:19:28 -0300

    turn synchronous countdown into async/callback countdown

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -10,8 +10,6 @@ const ErrorPage = () => {
 	const navigate = useNavigate()
 	const [countdown0, setCountdown0] = useState('')
 	const [countdown1, setCountdown1] = useState('')
-	const [countdown2, setCountdown2] = useState('')
-	const [countdown3, setCountdown3] = useState('')
 
 	let errormessage: string = ''
 	if (getUrlParamVal(url, 'error_description')) {
@@ -20,21 +18,19 @@ const ErrorPage = () => {
 	}
 
 	useEffect(() => {
-		setTimeout(() => {
-			setCountdown0('We will redirect you to the login page in ')
-		}, 1000)
+		setCountdown0('We will redirect you to the login page in ')
 		setTimeout(() => {
 			setCountdown1('3. ')
-		}, 2000)
-		setTimeout(() => {
-			setCountdown2('2. ')
-		}, 3000)
-		setTimeout(() => {
-			setCountdown3('1. ')
-		}, 4000)
-		setTimeout(() => {
-			navigate('/account/login')
-		}, 5000)
+			setTimeout(() => {
+				setCountdown1('3. 2. ')
+				setTimeout(() => {
+					setCountdown1('3. 2. 1. ')
+					setTimeout(() => {
+						navigate('/account/login')
+					}, 1000)
+				}, 1000)
+			}, 1000)
+		}, 200)
 	}, [navigate])
 	return (
 		<>
@@ -43,8 +39,6 @@ const ErrorPage = () => {
 			<p>
 				{countdown0}
 				{countdown1}
-				{countdown2}
-				{countdown3}
 			</p>
 		</>
 	)
