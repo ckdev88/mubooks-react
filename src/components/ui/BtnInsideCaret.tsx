0e799f63a0c00ab4aecfaa9d83410bf43d685b87
ckdev88 <ckdev88@outlook.com>
2024-09-11 10:12:25 -0300

    nano tweak

--- src/components/ui/BtnInsideCaret.tsx
+++ src/components/ui/BtnInsideCaret.tsx
@@ -1,4 +1,4 @@
 const BtnInsideCaret = () => {
-	return <button  className="btn-submit-inside-caret-right"></button>
+	return <button className="btn-submit-inside-caret-right"></button>
 }
 export default BtnInsideCaret
