353d3e9cdf3c83a474a4d823a7d2863806826db1
ckdev88 <ckdev88@outlook.com>
2024-09-16 20:16:13 -0300

    fix bug where finish reading a boot needs re-render to show finished date in saved-books page

--- src/components/BookStartedFinished.tsx
+++ src/components/BookStartedFinished.tsx
@@ -18,6 +18,20 @@ const BookStartedFinished = ({
 	const { userMyBooks, setUserMyBooks, setPopupNotification, userid, todaysDateInput } = useContext(AppContext)
 	const [dateStarted, setDateStarted] = useState<Book['date_reading']>(date_started)
 	const [dateFinished, setDateFinished] = useState<Book['date_finished']>(date_finished)
+	const [showStartedDate, setShowStartedDate] = useState<boolean>(false)
+	const [showFinishedDate, setShowFinishedDate] = useState<boolean>(false)
+
+	useEffect(() => {
+		if (date_finished === 0 || date_finished === undefined || date_finished === null) {
+			setShowStartedDate(true)
+			setShowFinishedDate(false)
+		} else {
+			setShowStartedDate(false)
+			setShowFinishedDate(true)
+		}
+		/* TODO: this needs some work, there might be (faulty) cases where dateFinished is falsy, but book.list is 3 or 4 */
+	}, [date_finished, userMyBooks, date_started, setShowStartedDate, setShowFinishedDate, dateFinished])
+
 	async function MyBooksUpdate(myBooksNew: Books) {
 		let msg: string
 		setUserMyBooks(myBooksNew)
@@ -35,7 +49,7 @@ const BookStartedFinished = ({
 
 	function changeDates(fieldName: string, fieldVal: number) {
 		if (fieldName === 'date_reading') setDateStarted(fieldVal)
-		else if (fieldName === 'date_finished') setDateFinished(fieldVal)
+		if (fieldName === 'date_finished') setDateFinished(fieldVal) // TODO: tweak: make else if
 
 		let myBooks: Books
 		if (userMyBooks === undefined) myBooks = []
@@ -76,30 +90,31 @@ const BookStartedFinished = ({
 			const eleDateFinished = document.getElementById('date_finished' + book_id) as HTMLInputElement | null
 			if (eleDateFinished !== null) eleDateFinished.value = convertDate(dateFinished, 'input')
 		}
-	}, [dateStarted, dateFinished, book_id])
+	}, [dateStarted, dateFinished, book_id, showStartedDate, showFinishedDate])
 
 	return (
 		<div className="book-started-finished">
 			<div>
-				<em className="btn-text">
-					<span className="icon icon-reading"></span>
-					<button
-						className="btn-calendar btn-text"
-						onClick={() => openCalendarPopUp('date_reading' + book_id)}
-					>
-						{dateStarted && convertDate(dateStarted, 'human')}
-					</button>
-				</em>
-
-				<input
-					tabIndex={-1}
-					id={'date_reading' + book_id}
-					name={'date_reading' + book_id}
-					type="date"
-					max={todaysDateInput}
-					className="calendar-hidden"
-					onChange={debounce(() => modifyDateReading('date_reading'), 100)}
-				/>
+				<>
+					<em className="btn-text">
+						<span className="icon icon-reading"></span>
+						<button
+							className="btn-calendar btn-text"
+							onClick={() => openCalendarPopUp('date_reading' + book_id)}
+						>
+							{dateStarted && convertDate(dateStarted, 'human')}
+						</button>
+					</em>
+					<input
+						tabIndex={-1}
+						id={'date_reading' + book_id}
+						name={'date_reading' + book_id}
+						type="date"
+						max={todaysDateInput}
+						className="calendar-hidden"
+						onChange={debounce(() => modifyDateReading('date_reading'), 100)}
+					/>
+				</>
 
 				{list > 2 && (
 					<>
@@ -109,7 +124,7 @@ const BookStartedFinished = ({
 								className="btn-calendar btn-text"
 								onClick={() => openCalendarPopUp('date_finished' + book_id)}
 							>
-								{dateFinished && convertDate(dateFinished, 'human')}
+								{date_finished && convertDate(date_finished, 'human')}
 							</button>
 						</em>
 						<input
@@ -120,7 +135,7 @@ const BookStartedFinished = ({
 							min={date_started && convertDate(date_started, 'input')}
 							max={todaysDateInput}
 							className="calendar-hidden"
-							onChange={debounce(() => modifyDateReading('date_finished'), 1000)}
+							onChange={debounce(() => modifyDateReading('date_finished'), 100)}
 						/>
 					</>
 				)}
