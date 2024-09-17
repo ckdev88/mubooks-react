fa0ec731544311fb8f2bb01faa07543266ca1995
ckdev88 <ckdev88@outlook.com>
2024-09-16 21:05:27 -0300

    add useclearbooks hook, is a bit hacky and lame, needs work

---
+++ src/hooks/useClearBooks.ts
@@ -0,0 +1,17 @@
+import { useContext } from 'react'
+import { MyBooksUpdate } from '../helpers/MyBooksHelpers'
+import { AppContext } from '../App'
+
+const useClearBooks = (): [() => void] => {
+	const { setUserMyBooks } = useContext(AppContext)
+	const clearnow = async (): Promise<void> => {
+		await MyBooksUpdate([])
+			.then(()=>setUserMyBooks([]))
+			.finally(() => console.log('cleared'))
+		// setUserMyBooks([])
+		// console.log('clear!')
+	}
+
+	return [clearnow]
+}
+export default useClearBooks
