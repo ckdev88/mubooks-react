33a34db5afa94f25f7414a88eed36cad37651bb3
ckdev88 <ckdev88@outlook.com>
2024-09-27 13:46:01 -0300

    make react router use pathnames, allow server to use it

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 )
