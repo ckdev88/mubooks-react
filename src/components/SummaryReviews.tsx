48b30575b380d2d5bdf6dbc56654e9b49bb99158
ckdev88 <ckdev88@outlook.com>
2024-09-30 04:27:58 -0300

    make ReviewQuote & ReviewText obsolete and removed them

--- src/components/SummaryReviews.tsx
+++ src/components/SummaryReviews.tsx
@@ -1,12 +1,11 @@
 import ReviewTropes from './ReviewTropes'
-import ReviewQuote from './ReviewQuote'
 import BookSummaryReview from './BookSummaryReview'
 
 export default function SummaryReviews({ currentPage, book }: { currentPage: Page; book: Book }) {
 	return (
 		<div className="reviews">
 			{currentPage === 'quoted' && (
-				<ReviewQuote book_id={book.id} book_review_fav_quote={book.review_fav_quote} />
+				<BookSummaryReview book_id={book.id} review_val={book.review_fav_quote} review_type="quote" />
 			)}
 			{(currentPage === 'finished' || currentPage === 'favorites' || currentPage === 'savedbooks') && (
 				<>
