0a5618d8aff3f1781da31988d808527423829beb
ckdev88 <ckdev88@outlook.com>
2024-10-17 08:40:02 -0300

    add message and action for when no tropes in books are found

--- src/components/TropesInMyBooks.tsx
+++ src/components/TropesInMyBooks.tsx
@@ -4,6 +4,7 @@ import { AppContext } from '../App'
 import BooksOverviewPage from '../routes/books/BooksOverviewPage'
 import { TropesPageContext } from '../routes/books/TropesPage'
 import { cleanAnchor } from '../helpers/cleanInput'
+import { Link } from 'react-router-dom'
 
 const TropesInMyBooks = ({ page }: { page: Page }) => {
 	const [isShowingTropesInMyBooks, setIsShowingTropesInMyBooks] = useState<boolean>(true)
@@ -12,6 +13,8 @@ const TropesInMyBooks = ({ page }: { page: Page }) => {
 	const [activeTrope, setActiveTrope] = useState<string>('')
 	const [tropeBooks, setTropeBooks] = useState<Books>([])
 	const tropesSet = new Set<string>()
+
+	const savedbookslink: Page = 'savedbooks'
 
 	userMyBooks.map((book) => {
 		if (book.review_tropes) book.review_tropes.map((reviewtrope) => tropesSet.add(reviewtrope.trim().toLowerCase()))
@@ -39,31 +42,38 @@ const TropesInMyBooks = ({ page }: { page: Page }) => {
 		<section className="section-badges">
 			<h2>
 				Tropes in my Books&nbsp;
-				<button
-					className={
-						isShowingTropesInMyBooks
-							? 'btn-text caret-right-toggle active wauto notext diblock'
-							: 'btn-text caret-right-toggle wauto notext diblock'
-					}
-					onClick={() => setIsShowingTropesInMyBooks(!isShowingTropesInMyBooks)}
-				></button>
+				{tropesArr.length > 0 && (
+					<button
+						className={
+							isShowingTropesInMyBooks
+								? 'btn-text caret-right-toggle active wauto notext diblock'
+								: 'btn-text caret-right-toggle wauto notext diblock'
+						}
+						onClick={() => setIsShowingTropesInMyBooks(!isShowingTropesInMyBooks)}
+					></button>
+				)}
 			</h2>
-
-			<div
-				className={isShowingTropesInMyBooks ? 'tropes clr expandable expanded' : 'tropes clr expandable collapsed'}
-				aria-expanded={isShowingTropesInMyBooks}
-			>
-				{tropesArr.map((trope, index) => {
-					let cn: string = 'btn-sm mb0 badge'
-					if (likedTropesLowercase.includes(trope.toLowerCase())) cn += ' cgreen'
-					else if (dislikedTropesLowercase.includes(trope.toLowerCase())) cn += ' cred'
-					return (
-							<button   key={cleanIndexKey(trope, index)} className={cn} onClick={() => showTropeBooks(trope)}>
+			{tropesArr.length > 0 ? (
+				<div
+					className={isShowingTropesInMyBooks ? 'tropes clr expandable expanded' : 'tropes clr expandable collapsed'}
+					aria-expanded={isShowingTropesInMyBooks}
+				>
+					{tropesArr.map((trope, index) => {
+						let cn: string = 'btn-sm mb0 badge'
+						if (likedTropesLowercase.includes(trope.toLowerCase())) cn += ' cgreen'
+						else if (dislikedTropesLowercase.includes(trope.toLowerCase())) cn += ' cred'
+						return (
+							<button key={cleanIndexKey(trope, index)} className={cn} onClick={() => showTropeBooks(trope)}>
 								{trope}
 							</button>
-					)
-				})}
-			</div>
+						)
+					})}
+				</div>
+			) : (
+				<div className="ml035">
+					No tropes in my books yet, want to add some? <Link to={savedbookslink}>Go to my books</Link>
+				</div>
+			)}
 			{tropeBooks.length > 0 && (
 				<>
 					<h2 style={{ position: 'relative' }}>
