6633d5c822f273db223cf334e70ba952f0a5e773
ckdev88 <ckdev88@outlook.com>
2024-10-21 14:41:07 -0300

    [loginpage] add Alpha version

--- src/routes/account/UserLoginPage.tsx
+++ src/routes/account/UserLoginPage.tsx
@@ -20,6 +20,7 @@ export default function UserLoginPage() {
 			<h1 id="welcome">
 				<img id="welcome-logo-img" src="/img/logo.svg" alt="" /> MuBooks
 			</h1>
+			<i className="dblock tcenter cbadgefg mt-1 mb1">Alpha version</i>
 			<div id="cards-draaideur" className="cards-draaideur">
 				<div className="axis">
 					<LoginCard />
