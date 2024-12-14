2d9d6f2a0748d77325955de0417e1660bf41544b
ckdev88 <ckdev88@outlook.com>
2024-12-14 00:25:24 -0300

    [pwa] finish initial super basic PWA setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
@@ -12,4 +11,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 		</BrowserRouter>
 	</React.StrictMode>
 )
-
