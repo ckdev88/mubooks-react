2e3f9ac04140246bef7c73c5a6fda135ad83a117
ckdev88 <ckdev88@outlook.com>
2024-12-28 18:32:23 -0300

    [bugreport] remove react state for form because not needed, add some js checking

--- src/components/SuggestionsForm.tsx
+++ src/components/SuggestionsForm.tsx
@@ -1,31 +1,24 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { AppContext } from '../App'
-
-interface FormState {
-	type: string
-	suggestion?: string
-	anythingElse?: string
-	userid: string
-	username?: string
-}
+import { cleanInput } from '../helpers/cleanInput'
 
 const SuggestionsForm: React.FC = () => {
 	const { userid } = useContext(AppContext)
-	const [formState, setFormState] = useState<FormState>({
-		type: 'suggestion',
-		userid: userid,
-	})
 	const [message, setMessage] = useState<string>('')
-
-	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		setFormState({
-			...formState,
-			[event.target.name]: event.target.value,
-		})
-	}
 
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
+		
+		// checking & sanitizing input
+		// TODO checking & sanitizing also in PHP, just to be sure
+		const formdata: FormData = new FormData(event.target as HTMLFormElement)
+		const formdata_suggestion: string | undefined = formdata.get('suggestion')?.toString().trim()
+		if (!formdata_suggestion || (formdata_suggestion && formdata_suggestion.length < 1)) return false
+const formdata_anythingElse: string | undefined = formdata.get('anythingElse')?.toString().trim()
+if (formdata_suggestion) formdata.set('suggestion', cleanInput(formdata_suggestion))
+		if (formdata_anythingElse !== undefined) formdata.set('anythingElse', cleanInput(formdata_anythingElse))
+		else formdata.set('anythingElse', '')
+
 		fetch('ProcessSuggestion.php', {
 			method: 'POST',
 			body: new FormData(event.target as HTMLFormElement),
@@ -35,34 +28,37 @@ const SuggestionsForm: React.FC = () => {
 			.catch((error) => console.error('Error:', error))
 	}
 
+	useEffect(() => {
+		document.getElementById('fsb_suggestion')?.focus()
+	}, [])
+
 	return (
 		<div>
 			{message === '' ? (
 				<>
+					<div className="h1">
+						What do you think?
+						<sub>Please let us know...</sub>
+					</div>
+					<div>
+						What do you think about MuBooks, and what could make it better?
+						<br />
+						Please tell us anything: a request, a complaint or a suggestion, we need your help to make this app as
+						friendly and helpful as possible for you.
+					</div>
+					<br />
 					<form onSubmit={handleSubmit}>
 						<label htmlFor="fsb_suggestion">
-							<div className="description">Please, let us know your ideas.</div>
-							<textarea
-								id="fsb_suggestion"
-								name="suggestion"
-								value={formState.suggestion || ''}
-								onChange={handleInputChange}
-								rows={5}
-							/>
+							<div className="description">Your ideas or suggestions</div>
+							<textarea id="fsb_suggestion" name="suggestion" rows={5} required />
 						</label>
 						<label htmlFor="fsb_anythingElse">
 							<div className="description">Anything else?</div>
-							<textarea
-								id="fsb_anythingElse"
-								name="anythingElse"
-								value={formState.anythingElse || ''}
-								onChange={handleInputChange}
-								rows={5}
-							/>
+							<textarea id="fsb_anythingElse" name="anythingElse" rows={5} />
 						</label>
 						<input type="hidden" name="userid" value={userid} />
 						<button className="btn-lg" type="submit">
-							Submit
+							Send
 						</button>
 					</form>
 				</>
