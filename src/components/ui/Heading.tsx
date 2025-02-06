e54d3306801dd4bfd33597792e7147f27eaaea4b
ckdev88 <ckdev88@outlook.com>
2025-02-06 09:05:57 -0300

    refactor: simplify Heading component, apply Storybook

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,38 +1,39 @@
-import { ReactNode } from 'react'
+// OPTIMIZE themes_icons: apply smart way of determining light or dark icon based on current theme, currently turned off via GLOBALS.headingIconsEnabled=false
 import { useContext } from 'react'
 import { AppContext } from '../../App'
-
-// OPTIMIZE themes_icons: apply smart way of determining light or dark icon based on current theme, currently turned off via GLOBALS.headingIconsEnabled=false
-const Heading = ({
-	el = 'h1',
-	text,
-	icon,
-	sub,
-	span,
-}: {
+interface HeadingProps {
 	el?: 'h1' | 'h2' | 'adder-header'
 	text: string
 	icon?: string
-	sub?: ReactNode
-	span?: ReactNode
-}) => {
-	const { GLOBALS } = useContext(AppContext)
-	if (GLOBALS.headingIconsEnabled && icon) {
-		return (
-			<div className={el + ' ' + `${el}-with-icon`}>
-				<div>
-					{text} {icon && <img src={`/img/${icon}`} alt="" className="h1-icon" />}
+	sub?: string
+}
+const Heading = ({ el = 'h1', text, icon, sub }: HeadingProps) => {
+	if (icon) {
+		const { GLOBALS } = useContext(AppContext)
+		if (GLOBALS.headingIconsEnabled && icon) {
+			return (
+				<div className={el + ' ' + `${el}-with-icon`}>
+					<div>
+						{text}{' '}
+						{icon && (
+							<img
+								src={`/img/${icon}`}
+								alt=""
+								className="h1-icon"
+							/>
+						)}
+					</div>
+					{sub && el !== 'adder-header' && <sub>{sub}</sub>}
+					{el === 'adder-header' && <span>›</span>}
 				</div>
-				{sub && <sub>{sub}</sub>}
-				{span && <span>{span}</span>}
-			</div>
-		)
+			)
+		}
 	}
 	return (
 		<div className={el}>
 			{text}
-			{sub && <sub>{sub}</sub>}
-			{span && <span>{span}</span>}
+			{sub && el !== 'adder-header' && <sub>{sub}</sub>}
+			{el === 'adder-header' && <span>›</span>}
 		</div>
 	)
 }
