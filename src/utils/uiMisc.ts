58411cd02e7e270a8cc9d3fec0d97cf1abded82b
ckdev88 <ckdev88@outlook.com>
2025-06-20 03:30:08 +0200

    ui: disable fadeout-effect for search-page

--- src/utils/uiMisc.ts
+++ src/utils/uiMisc.ts
@@ -1,7 +1,7 @@
 export default function fadeout(book_id: Book["id"]): void {
-    // TODO see if conditionals below are still applicable
     /** Temporary Current Page, taken from url */
-    // const tcp = window.location.pathname.replace("/", "")
+    const tcp = window.location.pathname.replace("/", "")
+    // TODO see if conditionals below are still applicable 
     // if (
     //     (tcp === "reading" && targetList !== 2) ||
     //     (tcp === "wishlist" && targetList !== 1) ||
@@ -9,5 +9,7 @@ export default function fadeout(book_id: Book["id"]): void {
     //     (tcp === "finished" && targetList !== 3 && targetList !== 4) ||
     //     (tcp === "tossed" && targetList > 0)
     // ) {
-    document.getElementById(`bookSummaryTransitioner${book_id}`)?.classList.add("fadeout")
+    if (tcp !== "search") {
+        document.getElementById(`bookSummaryTransitioner${book_id}`)?.classList.add("fadeout")
+    }
 }
