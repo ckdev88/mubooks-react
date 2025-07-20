013fe33afb3967e3eb04a05353a7b05014ad16a8
ckdev88 <ckdev88@outlook.com>
2025-07-20 18:36:22 +0200

    ui: prevent "no stats" inleiding flash in StatisticsPage

--- src/routes/books/StatisticsPage.tsx
+++ src/routes/books/StatisticsPage.tsx
@@ -7,14 +7,11 @@ import { motion } from "motion/react"
 
 const StatisticsPage = () => {
     const { userMyBooks, GLOBALS } = useContext(AppContext)
-    const [years, setYears] = useState<number[]>([])
-    const [hasStats, setHasStats] = useState<boolean>(false)
+
+    const [years, setYears] = useState<number[] | undefined>(undefined)
+
     useEffect(() => {
-        const { yearArr } = getFinishedBooksStatsYears(userMyBooks)
-        if (yearArr.length > 0) {
-            setYears(yearArr)
-            setHasStats(true)
-        }
+        userMyBooks !== undefined && setYears(getFinishedBooksStatsYears(userMyBooks).yearArr)
     }, [userMyBooks])
 
     return (
@@ -27,35 +24,39 @@ const StatisticsPage = () => {
             }}
             animate={{ opacity: 1 }}
         >
-            {hasStats ? (
+            <Heading
+                text="Statistics"
+                sub="See more about your book reading journey"
+                icon="icon-statistics.svg"
+            />
+            {years !== undefined ? (
                 <>
-                    {years.map((y) => {
-                        const filteredByYear = userMyBooks?.filter(
-                            (b) =>
-                                b.date_finished !== undefined &&
-                                Math.floor(b.date_finished / 10000) === y,
-                        )
-                        return (
-                            <StatisticsYear
-                                myBooksArr={filteredByYear}
-                                year={y}
-                                key={`statisticsPageYear${y}`}
-                            />
-                        )
-                    })}
+                    {years.length > 0 ? (
+                        <div>
+                            {years.map((y) => {
+                                const filteredByYear = userMyBooks?.filter(
+                                    (b) =>
+                                        b.date_finished !== undefined &&
+                                        Math.floor(b.date_finished / 10000) === y,
+                                )
+                                return (
+                                    <StatisticsYear
+                                        myBooksArr={filteredByYear}
+                                        year={y}
+                                        key={`statisticsPageYear${y}`}
+                                    />
+                                )
+                            })}
+                        </div>
+                    ) : (
+                        <p>
+                            No stats yet, stats are generated when you finish reading a book and
+                            when you rate the book you read.
+                        </p>
+                    )}
                 </>
             ) : (
-                <>
-                    <Heading
-                        text="Statistics"
-                        sub="See more about your book reading journey"
-                        icon="icon-statistics.svg"
-                    />
-                    <p>
-                        No stats yet, stats are generated when you finish reading a book and when
-                        you rate the book you read.
-                    </p>
-                </>
+                <span className="loader-dots" />
             )}
         </motion.div>
     )
