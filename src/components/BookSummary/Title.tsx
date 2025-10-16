044381f8c25e7bbc3502bf75dd7fc5ea3e1eaf2c
ckdev88 <ckdev88@outlook.com>
2025-10-16 13:46:11 +0200

    fix: do not show null book_first_publish_year

--- src/components/BookSummary/Title.tsx
+++ src/components/BookSummary/Title.tsx
@@ -31,10 +31,13 @@ const BookSummaryTitle = ({
     return (
         <div className="h2">
             {book_title_short}{" "}
-            {book_first_publish_year &&
+            {book_first_publish_year && book_first_publish_year > 0 ? (
                 (currentPage === "search" ||
                     currentPage === "addbook" ||
-                    currentPage === "wishlist") && <sup>({book_first_publish_year})</sup>}
+                    currentPage === "wishlist") && <sup>({book_first_publish_year})</sup>
+            ) : (
+                <> </>
+            )}
             <sub>
                 <BookAuthorList book_id={book_id} book_author_name={book_author_name} />
             </sub>
